bddc1a81bddd46c230b09c072b55e98d62ea8e9f
ttchef <ruro0.edit@gmail.com>
2025-07-05 09:52:14 +0200

    simple string loading

---
+++ dings.h
@@ -0,0 +1,84 @@
+
+#ifndef DINGS_H
+#define DINGS_H 
+
+#define DINGS_IMPL // Temp only for development
+#define DS_SMALL_STRING_CAPACITY 15
+
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h> 
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint32_t length;
+    bool is_heap;
+    union {
+        char stack_data[DS_SMALL_STRING_CAPACITY + 1]; 
+        char* heap_data;
+    };
+} ds_String;
+
+ds_String* ds_init_string(const char* string);
+void ds_free_string(ds_String* string);
+const char* ds_get_string_ptr(ds_String* string);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DINGS_H
+
+#ifdef DINGS_IMPL
+
+ds_String* ds_init_string(const char *string) {
+    size_t length = strlen(string);
+    ds_String* str = (ds_String*)malloc(sizeof(ds_String));
+    if (!str) {
+        fprintf(stderr, "[ERROR] Couldnt allocate enough memory for string: %s\n", string);
+        return NULL;
+    }
+
+    str->length = length;
+
+    if (length <= DS_SMALL_STRING_CAPACITY) {
+        str->is_heap = false;
+        memcpy(str->stack_data, string, length + 1);
+    }
+    else {
+        str->is_heap = true;
+        str->heap_data = (char*)malloc(length + 1);
+        if (!str->heap_data) {
+            fprintf(stderr, "[ERROR] Couldnt allocate heap data for string: %s\n", string);
+            free(str);
+            return NULL;
+        }
+        memcpy(str->heap_data, string, length + 1);
+    }
+    
+
+    return str;
+}
+
+void ds_free_string(ds_String *string) {
+    if (string->is_heap && string->heap_data) {
+        free(string->heap_data);
+    }
+    free(string);
+}
+
+const char* ds_get_string_ptr(ds_String* string) {
+    if (string->is_heap && string->heap_data) {
+        return string->heap_data;
+    }
+    return string->stack_data;
+}
+
+#endif
+
+
--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 
 #include <stdio.h> 
 
+#define DINGS_IMPL
+#include "dings.h"
+
 int main() {
-    printf("Hello World\n");
+    ds_String* string = ds_init_string("Hello World");
+        
+    printf("%s\n", ds_get_string_ptr(string));
+
+    ds_free_string(string);
     return 0;
 }
 
